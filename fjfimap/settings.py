06c93d857691e71c55195a4421f0170b8353145c
jiri99 <44276136+jiri99@users.noreply.github.com>
2022-04-26 08:37:22 +0200

    Added html file for homepage

--- fjfimap/settings.py
+++ fjfimap/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'fjfimap.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./frontend/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- frontend/views.py
+++ frontend/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello world</h1>")
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
