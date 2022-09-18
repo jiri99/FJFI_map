68d3d0bd453a1ea7a7475071b74c2c23cc728e08
jiri.nabelek <jiri.nabelek@skodagroup.com>
2022-09-18 21:51:40 +0200

    Views and settings modified.

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Building
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "frontend/home.html", {})
+    queryset = Building.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "frontend/home.html", context)
