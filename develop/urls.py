5e76aadb7d19e74a3d236ce49de123f572003f96
jiri.nabelek <jiri.nabelek@skodagroup.com>
2022-11-09 21:02:34 +0100

    New version initial commit

--- develop/urls.py
+++ develop/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from frontend.views import home_view, homepage_view, display_view, list_view
+from develop.views import home_view, homepage_view, display_view, list_view
 
 
 app_name = 'frontend'
--- fjfimap/urls.py
+++ fjfimap/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from frontend.views import home_view, homepage_view, display_view, list_view
+from develop.views import home_view, homepage_view, display_view, list_view
 
 urlpatterns = [
     path('', home_view, name='home'),
