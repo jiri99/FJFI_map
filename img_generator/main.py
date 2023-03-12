b2f79959098f727ef9dcb9cd0672f47e6c2152c9
jiri.nabelek@skodagroup.com <jiri.nabelek@skodagroup.com>
2023-03-12 15:21:23 +0100

    Image generator created.

--- img_generator/main.py
+++ img_generator/main.py
@@ -7,11 +7,51 @@ Created on Fri Nov 25 10:59:52 2022
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from PIL import Image
+from PIL import Image, ImageColor
 import cv2
 import yaml
 
+img_output_path = "./../develop/db_images"
 config_path = "./config.yaml"
+
+def display(img):
+    plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+
+def prerocess_cordinates(cordinates):
+    cord_str = cordinates.split(",")
+    cord_all = [int(n) for n in cord_str]
+    cord = np.zeros([int(len(cord_all)/2),2], dtype=int)
+    for i in range(0, len(cord_all), 2):
+        cord[int(i/2),:] = cord_all[i:(i+2)]
+    return cord
+    
+def shadow_area(image_floor, room_data):    
+    mask = np.zeros(image_floor.shape, dtype=image_floor.dtype)
+    cordinates = prerocess_cordinates(room_data["cordinates"])
+    cv2.fillConvexPoly(mask, cordinates, 128)
+    
+    mask_bool = mask > 0
+    im = np.zeros_like(image_floor)
+    for i in range(image_floor.shape[0]):
+        for j in range(image_floor.shape[1]):
+            if(mask_bool[i,j] == True and image_floor[i,j] > 128):
+                im[i,j] = 128
+            else:
+                im[i,j] = image_floor[i,j]
+    img = Image.fromarray(im)
+    return img
+
+def cropp(img, cordinates, x_size = 300, y_size = 300):
+    x_middle = np.mean(cordinates[:,0])
+    y_middle = np.mean(cordinates[:,1])
+    
+    cropped_img = img.crop((x_middle-x_size, y_middle-y_size, x_middle+x_size, y_middle+y_size))
+    
+    plt.imshow(cropped_img, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+
+    
 
 with open(config_path) as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
@@ -19,7 +59,67 @@ with open(config_path) as f:
 for building in list(data.keys()):
     for floor in list(data[building].keys()):
         # print(data[building][floor]["path"])
-        data[building][floor]["img"] = Image.open(data[building][floor]["path"], "r")
+        # data[building][floor]["img"] = np.array(Image.open(data[building][floor]["path"]))
+        data[building][floor]["img"] = cv2.imread(data[building][floor]["path"],0)
+        
+
+image_floor = data["Brehova"]["1.patro"]["img"]
+# image_floor = cv2.cvtColor(data["Brehova"]["1.patro"]["img"], cv2.COLOR_BGR2RGB)
+room_name = "B-101"
+room_data = data["Brehova"]["1.patro"]["classes"][room_name]
+
+# %%
+
+# cv2.imshow('Extracted Image', image_floor)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+mask = np.zeros(image_floor.shape, dtype=image_floor.dtype)
+
+cordinates = prerocess_cordinates(room_data["cordinates"])
+
+cv2.fillConvexPoly(mask, cordinates, 128)
+# cv2.fillConvexPoly(mask, cordinates, (128,128,128,255))
+
+mask_bool = mask > 0
+
+im = np.zeros_like(image_floor)
+
+for i in range(image_floor.shape[0]):
+    for j in range(image_floor.shape[1]):
+        if(mask_bool[i,j] == True and image_floor[i,j] > 128):
+        # if(mask_bool[i,j,0] == True and image_floor[i,j,0] == 255):
+            im[i,j] = 128
+            # im[i,j,:] = [255,255,255,1]
+        else:
+            im[i,j] = image_floor[i,j]
+            # im[i,j,:] = image_floor[i,j,:]
+
+# cv2.imshow('Extracted Image', im)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+img = Image.fromarray(im)
+
+plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+plt.show()
+
+img.save("pokus.jpg")
+
+#%%
+# spočítat průmměr souřadnic a udělat zoom!!!
+
+x_size = 300
+y_size = 350
+
+x_middle = np.mean(cordinates[:,0])
+y_middle = np.mean(cordinates[:,1])
+
+cropped_img = img.crop((x_middle-x_size, y_middle-y_size, x_middle+x_size, y_middle+y_size))
+
+plt.imshow(cropped_img, cmap='gray', vmin=0, vmax=255)
+plt.show()
+
 
 # path = "./images/brehova/patro_1.png"
 
