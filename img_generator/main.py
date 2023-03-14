4c86876e97d7262cee736713f15efdaef382b677
jiri.nabelek@skodagroup.com <jiri.nabelek@skodagroup.com>
2023-03-15 00:32:38 +0100

    Room skip added to image generator

--- img_generator/main.py
+++ img_generator/main.py
@@ -15,6 +15,7 @@ from img_generator import generate_image
 
 img_output_path = "./../develop/db_images/"
 config_path = "./config.yaml"
+config_path_out = "./config_out.yaml"
 id_counter = 18
 
 with open(config_path) as f:
@@ -27,20 +28,25 @@ for building in list(data.keys()):
 for building in list(data.keys()):
     for floor in list(data[building].keys()):
         for room in list(data[building][floor]["classes"].keys()):
-            generate_image(data[building][floor]["img"], data[building][floor]["classes"][room], room, img_output_path)
-            insert_into_database_room(id_counter, data[building][floor]["classes"][room]["name"], "", 1, img_output_path + room + ".jpg", data[building][floor]["classes"][room]["cordinates"])
-            roomtype_id = 2
-            if(data[building][floor]["classes"][room]["type"] == "class"):
-                insert_into_database_roomtype(id_counter, id_counter, 2)
-            elif(data[building][floor]["classes"][room]["type"] == "important"):
-                insert_into_database_roomtype(id_counter, id_counter, 4)
-            elif(data[building][floor]["classes"][room]["type"] == "wc"):
-                insert_into_database_roomtype(id_counter, id_counter, 5)
+            if(not data[building][floor]["classes"][room]["cordinates"] == ""):
+                if(data[building][floor]["classes"][room]["saved"] == 0):
+                    generate_image(data[building][floor]["img"], data[building][floor]["classes"][room], room, img_output_path)
+                    insert_into_database_room(id_counter, data[building][floor]["classes"][room]["name"], "", 1, img_output_path + room + ".jpg", data[building][floor]["classes"][room]["cordinates"])
+                    if(data[building][floor]["classes"][room]["type"] == "class"):
+                        insert_into_database_roomtype(id_counter, id_counter, 2)
+                    elif(data[building][floor]["classes"][room]["type"] == "important"):
+                        insert_into_database_roomtype(id_counter, id_counter, 4)
+                    elif(data[building][floor]["classes"][room]["type"] == "wc"):
+                        insert_into_database_roomtype(id_counter, id_counter, 5)
+                    else:
+                        insert_into_database_roomtype(id_counter, id_counter, 6)    
+                    data[building][floor]["classes"][room]["saved"] = 1
+                    id_counter += 1
+                else:
+                    continue
             else:
-                insert_into_database_roomtype(id_counter, id_counter, 6)    
-            data[building][floor]["classes"][room]["saved"] = 1
-            id_counter += 1
+                continue
 
-
-
+with open(config_path_out, "w") as file:
+    documents = yaml.dump(data, file)
 
